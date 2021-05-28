12dd8b8b5ec7535cc0bb07be70eefc0065f5b39d
Pouapheng Yang <phewy06@yahoo.com>
2021-05-28 17:42:20 -0500

    first setup

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,29 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = [];
+
+function addItem(item) {
+  const name = { item: item, qty: 1 }
+  basket.push(item);
+
+
+}
+
+function listItems() {
+  console.log(`You have ${basket.length} in basket`);
+  for (let i = 0; i < basket.length; i += 1){
+  }
+
+}
+
+function empty() {
+
+}
+
+addItem('Fishing pole')
+addItem('Fishing reel')
+addItem('Fishing lure')
+addItem('Fishing line')
+
+listItems()
